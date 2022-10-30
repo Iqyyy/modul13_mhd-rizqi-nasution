ac72867bfa164091f891cc71a5dcca795fe55c57
Iqyyy <m.fahrin191@gmail.com>
2022-10-30 20:28:38 +0700

    update

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div class="footer">
+            <p>
+                Mhd Rizqi Nasution | Netlab Bookstore | 320200401013
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import './Title.css';
+
+function Title() {
+    return(
+        <header class="header">
+            <script src="https://kit.fontawesome.com/bb9d21d155.js" crossorigin="anonymous"></script>
+            <div class="header-1">
+                <a href=" " class="logo"><i class="fa-solid fa-book">Netlab Bookstore</i></a>
+                <p>Toko Buku Paling Lengkap!</p>
+            </div> 
+        </header>
+    );
+}
+
+export default Title;
